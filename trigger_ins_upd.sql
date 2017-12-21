6100ce7e84f09da0551386bce580b7b7cc3ebca8
Salvatore Fiandaca <pigrecoinfinito@gmail.com>
2017-12-21 18:46:58 +0100

    Create trigger_ins_upd.sql

---
+++ trigger_ins_upd.sql
@@ -0,0 +1,79 @@
+CREATE TABLE punti
+(pk_p INTEGER PRIMARY KEY autoincrement NOT NULL,
+nome_strada TEXT,
+data_ins datetime,
+note TEXT,
+distanza DOUBLE);
+
+SELECT AddGeometryColumn('punti','geom',3004,'POINT',2);
+
+CREATE TABLE strade
+(pk_l INTEGER PRIMARY KEY autoincrement NOT NULL,
+nome_strada TEXT
+);
+
+SELECT AddGeometryColumn('strade','geom',3004,'LINESTRING',2);
+
+-- popola i campi data_ins, distanza e nome_strada appena inserisco la geometria del punto
+
+CREATE TRIGGER ins_punti AFTER INSERT ON punti
+BEGIN
+   UPDATE punti SET data_ins = DATETIME ('NOW')
+   WHERE rowid = new.rowid ;
+
+   UPDATE punti SET distanza = 
+(
+   select j.dista
+   from 
+   (select NEW.pk_p,distance(l.geom, NEW.geom) as dista
+   from punti as  p, strade as l
+   group by NEW.pk_p
+   having min ( distance(l.geom, NEW.geom)) AND punti.ROWID=NEW.ROWID
+   ) j
+) 
+WHERE punti.ROWID=NEW.ROWID;
+
+   UPDATE punti SET nome_strada =
+(
+   select k.nome_strada
+   from 
+   (select NEW.pk_p,l.nome_strada
+   from punti as  p, strade as l
+   group by NEW.pk_p
+   having min ( distance(l.geom, NEW.geom)) AND punti.ROWID=NEW.ROWID
+   ) k
+)
+WHERE punti.ROWID=NEW.ROWID;
+END;
+
+-- aggiorna i campi data_ins, distanza e nome_strada appena modifico la SOLA geometria del punto
+
+CREATE TRIGGER upd_punti AFTER UPDATE OF geom ON punti
+BEGIN
+   UPDATE punti SET data_ins = DATETIME ('NOW')
+   WHERE rowid = new.rowid ;
+
+   UPDATE punti SET distanza = 
+(
+   select j.dista
+   from 
+   (select NEW.pk_p,distance(l.geom, NEW.geom) as dista
+   from punti as  p, strade as l
+   group by NEW.pk_p
+   having min ( distance(l.geom, NEW.geom)) AND punti.ROWID=NEW.ROWID
+   ) j
+) 
+WHERE punti.ROWID=NEW.ROWID;
+
+   UPDATE punti SET nome_strada =
+(
+   select k.nome_strada
+   from 
+   (select NEW.pk_p,l.nome_strada
+   from punti as  p, strade as l
+   group by NEW.pk_p
+   having min ( distance(l.geom, NEW.geom)) AND punti.ROWID=NEW.ROWID
+   ) k
+)
+WHERE punti.ROWID=NEW.ROWID;
+END;
